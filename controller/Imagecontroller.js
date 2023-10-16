60f8d99adc7a2eceb60133307b8ef943a6bf4edb
GAURAVCHOUHA <130651572+GAURAVCHOUHA@users.noreply.github.com>
2023-10-16 11:15:18 +0530

    image

--- controller/Imagecontroller.js
+++ controller/Imagecontroller.js
@@ -4,7 +4,7 @@ Routes.post("/",async(req,res)=>{
     console.log(req.files);
     console.log(req.body);
     let filedata=req.files.image;
-    filedata.mv(path.resolve()+"/assets/images/"+filedata.name,(err)=>{
+    filedata.mv(path.resolve()+"/assets/blog-data/"+filedata.name,(err)=>{
         if(err){
             console.log(err);
         }
